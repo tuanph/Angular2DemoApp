6a2e6684840fde0147a6112ed2d0a04d0bb03d7c
tuanph_homepc <tuan.phamhoang.2506@gmail.com>
2016-11-01 20:40:35 +0700

    Finish beta without profile

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from "angular2/core";
 
 @Component({
     selector: "home",
-    templateUrl: "app/home/home.component.html"
+    templateUrl: "app/home/home.component.html",
+    styleUrls :["app/home/home.styles.css"]
 })
 export class HomeComponent {
 
